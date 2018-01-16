ee4d4d06a07e2f434244449469c5342f29bed0b2
Lindsay Waldrop <lindsay.waldrop@nmt.edu>
2018-01-16 15:07:56 -0700

    Initial commit

---
+++ parameterfile.h
@@ -0,0 +1,49 @@
+
+// Struct that holds all input parameters from a file
+struct ParameterFile {
+
+	// List expected parameters and type here
+	double L1;
+	int N1;
+        double Let;
+	double tdiameter;
+	double tR2;
+	double pamp;
+	double freq;
+        double iposn;
+        
+	// Debugging function to print the values of parameters
+  //	void print(){
+	  //		cout <<"L1 = " <<L1 <<endl;
+	  //	cout <<"N1 = " <<N1 <<endl;
+	  //	cout <<"Let = " <<Let <<endl;
+	  //	cout <<"diameter = " <<diameter <<endl;
+	  //	}
+
+	// Set parameter values given input file name
+	void set(string & param_file){
+
+	        ifstream fsin(param_file.c_str());
+		string tmp_line;
+
+		while (getline(fsin, tmp_line)){
+			istringstream ssin(tmp_line.substr(tmp_line.find("=") + 1 ));
+			if (tmp_line.find("L1") != -1){ ssin >> L1; }
+			else if (tmp_line.find("N1") != -1){ ssin >> N1; }
+			else if (tmp_line.find("Let") != -1){ ssin >> Let; }
+			else if (tmp_line.find("tdiameter") != -1){ ssin >> tdiameter; }
+			else if (tmp_line.find("tR2") != -1){ ssin >> tR2; }
+			else if (tmp_line.find("pamp") != -1){ ssin >> pamp; }
+			else if (tmp_line.find("freq") != -1){ ssin >> freq; }
+			else if (tmp_line.find("iposn") != -1){ ssin >> iposn; }
+			else {
+				cout <<"Parameter '" <<tmp_line <<"' not found!" <<endl;
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+
+	// Declare constructor and descructor
+	//ParameterFile();
+	//~ParameterFile();
+};
---
+++ slurmjob.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Mimicking SLURM scheduler for use with Remora
+#
+
+SLURM_NODELIST
+PE_HOSTFILE
+
