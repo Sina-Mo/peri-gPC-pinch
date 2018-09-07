89d842ae0a02062f664e9aed93a2d619d4793cc1
Lindsay Waldrop <lindsay.waldrop@nmt.edu>
2018-09-07 12:31:08 -0400

    Changes to setup batch files for running on Dogwood

--- setrunperijobs.sh
+++ setrunperijobs.sh
@@ -10,12 +10,14 @@ do
 
 second=$((countby*i))
 first=$((second-(countby-1)))
+name=$first-$second
 echo Print $first to $second runs. 
 
-awk -v var="$first" 'NR==8 {$0="startrun="'"var"'""} 1' runperipinch.job > temp${i}
-awk -v var="$second" 'NR==9 {$0="endrun="'"var"'""} 1' temp${i} > go${first}to${second}.job
+awk -v var="$first" 'NR==8 {$0="startrun="'"var"'""} 1' runperipinch.job > temp${i}1_1
+awk -v var="$second" 'NR==9 {$0="endrun="'"var"'""} 1' temp${i}1_1 > temp${i}1_2
+awk -v var="$name" 'NR==2 {$0="#SBATCH --job-name=r"'"var"'""} 1' temp${i}1_2 > go${first}to${second}.job
 
-rm temp${i}
+rm temp${i}1_1 temp${i}1_2
 
 #sbatch go${first}to${second}.job
 
